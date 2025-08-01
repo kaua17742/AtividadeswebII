7620b4304211d82db6fbdf02ec5389729568bb21
kaua17742 <kjfm@discente.ifpe.edu.br>
2025-08-01 12:29:46 -0300

    atividade_5.2

--- atividade_5-2/routes/web.php
+++ atividade_5-2/routes/web.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
-
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\PublisherController;
 Route::get('/', function () {
     return view('welcome');
 });
@@ -11,3 +12,5 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('categories', CategoryController::class);
+Route::resource('authors', AuthorController::class);
+Route::resource('publishers', PublisherController::class);
